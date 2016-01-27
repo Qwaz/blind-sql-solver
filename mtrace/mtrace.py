d9e41cbf6f1e7c351d9eedfc90ee298e22441a7c
Qwaz <qwazpia@gmail.com>
2016-01-27 18:13:38 +0900

    fix arg parsing error

--- mtrace/mtrace.py
+++ mtrace/mtrace.py
@@ -4,5 +4,4 @@ import sys
 env = os.environ
 env['LD_PRELOAD'] = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib_mtrace')
 
-os.execve(sys.argv[1], sys.argv[2:], env)
-
+os.execve(sys.argv[1], sys.argv[1:], env)
